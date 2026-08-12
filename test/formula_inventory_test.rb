06bd2af419e41e560259758dc2dcabc15aa39245
Jonathan Baldie <jon@jonbaldie.com>
2026-08-12 17:02:18 +0100

    Require the complete formula inventory (#13)

---
+++ test/formula_inventory_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "fileutils"
+require "minitest/autorun"
+require "tmpdir"
+require_relative "../script/formula_inventory"
+
+class FormulaInventoryTest < Minitest::Test
+  def test_accepts_exactly_one_formula_for_each_registered_tool
+    Dir.mktmpdir do |directory|
+      ToolRegistry.names.each { |tool| FileUtils.touch(File.join(directory, "#{tool}.rb")) }
+
+      FormulaInventory.verify!(directory)
+    end
+  end
+
+  def test_rejects_a_missing_formula
+    Dir.mktmpdir do |directory|
+      (ToolRegistry.names - ["messrust"]).each do |tool|
+        FileUtils.touch(File.join(directory, "#{tool}.rb"))
+      end
+
+      error = assert_raises(FormulaInventory::Mismatch) do
+        FormulaInventory.verify!(directory)
+      end
+      assert_includes error.message, "missing: messrust"
+    end
+  end
+
+  def test_rejects_an_unregistered_formula
+    Dir.mktmpdir do |directory|
+      ToolRegistry.names.each { |tool| FileUtils.touch(File.join(directory, "#{tool}.rb")) }
+      FileUtils.touch(File.join(directory, "other.rb"))
+
+      error = assert_raises(FormulaInventory::Mismatch) do
+        FormulaInventory.verify!(directory)
+      end
+      assert_includes error.message, "unexpected: other"
+    end
+  end
+end
